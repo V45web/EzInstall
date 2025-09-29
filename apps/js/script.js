4513f782abea92e8883cca5352753ffb017b7666
V 45 <sanagkerkar45@gmail.com>
2025-09-29 19:02:12 +0000

    bo

--- apps/js/script.js
+++ apps/js/script.js
@@ -1,6 +1,36 @@
-const code = document.getElementById("code").innerText;
+const ubuntu = document.getElementById("ubuntu").innerText;
+const debian = document.getElementById("debian").innerText;
+const arch = document.getElementById("arch").innerText;
+const manjaro = document.getElementById("manjaro").innerText;
+const fedora = document.getElementById("fedora").innerText;
+const mint = document.getElementById("mint").innerText;
+const kali = document.getElementById("kali").innerText;
 
-function copycode(){
-  navigator.clipboard.writeText(code)
+function copyubuntu(){
+  navigator.clipboard.writeText(ubuntu)
+  alert("Code is copy!")
+}
+function copydebian(){
+  navigator.clipboard.writeText(ubuntu)
+  alert("Code is copy!")
+}
+function copyarch(){
+  navigator.clipboard.writeText(ubuntu)
+  alert("Code is copy!")
+}
+function copymanjaro(){
+  navigator.clipboard.writeText(ubuntu)
+  alert("Code is copy!")
+}
+function copyfedora(){
+  navigator.clipboard.writeText(ubuntu)
+  alert("Code is copy!")
+}
+function copymint(){
+  navigator.clipboard.writeText(ubuntu)
+  alert("Code is copy!")
+}
+function copykali(){
+  navigator.clipboard.writeText(ubuntu)
   alert("Code is copy!")
 }
