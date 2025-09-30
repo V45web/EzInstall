b0bdfdc01cfb6c903002e50363891c22685ec5ef
V 45 <sanagkerkar45@gmail.com>
2025-09-30 09:42:26 +0000

    up

--- apps/js/script.js
+++ apps/js/script.js
@@ -1,36 +1,6 @@
-const ubuntu = document.getElementById("ubuntu").innerText;
-const debian = document.getElementById("debian").innerText;
-const arch = document.getElementById("arch").innerText;
-const manjaro = document.getElementById("manjaro").innerText;
-const fedora = document.getElementById("fedora").innerText;
-const mint = document.getElementById("mint").innerText;
-const kali = document.getElementById("kali").innerText;
-
-function copyubuntu(){
-  navigator.clipboard.writeText(ubuntu)
-  alert("Code is copy!")
-}
-function copydebian(){
-  navigator.clipboard.writeText(ubuntu)
-  alert("Code is copy!")
-}
-function copyarch(){
-  navigator.clipboard.writeText(ubuntu)
-  alert("Code is copy!")
-}
-function copymanjaro(){
-  navigator.clipboard.writeText(ubuntu)
-  alert("Code is copy!")
-}
-function copyfedora(){
-  navigator.clipboard.writeText(ubuntu)
-  alert("Code is copy!")
-}
-function copymint(){
-  navigator.clipboard.writeText(ubuntu)
-  alert("Code is copy!")
-}
-function copykali(){
-  navigator.clipboard.writeText(ubuntu)
-  alert("Code is copy!")
+function copyToClipboard(id) {
+  const text = document.getElementById(id).innerText;
+  navigator.clipboard.writeText(text)
+    .then(() => alert('Copied to clipboard!'))
+    .catch(err => alert('Failed to copy: ' + err));
 }
